5c83c2c1a794c1391d2dd9403344fcdb3fa586f3
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-04 17:39:48 +0800

    create update and delete route controller

--- controller/SubjectController.js
+++ controller/SubjectController.js
@@ -11,8 +11,12 @@ const handleErrors = (err) => {
 };
 
 module.exports.subject_all_get = async (req, res) => {
-    const subjects = await Subject.find();
-    res.json(subjects);
+    try {
+        const subjects = await Subject.find();
+        res.json(subjects);
+    } catch (err) {
+        console.log(err);
+    }
 };
 module.exports.subject_post = async (req, res) => {
     const { name, code, professor, schedule } = req.body.data;
@@ -32,4 +36,39 @@ module.exports.subject_post = async (req, res) => {
         handleErrors(err);
         res.status(400).send("Error in the server");
     }
+};
+
+module.exports.subject_update_post = (req, res) => {
+    const { name, code, professor, schedule } = req.body.data;
+    const { id } = req.params;
+
+    const updatedSubject = {
+        name,
+        code,
+        professor,
+        schedule,
+    };
+    
+    Subject.findByIdAndUpdate(id, updatedSubject, (err, doc) => {
+        if (!err) {
+            console.log("Document Updated");
+            res.send(doc);
+        } else {
+            console.log(err);
+        }
+    });
+};
+
+module.exports.subject_delete = (req, res) => {
+    const { id } = req.params;
+
+    Subject.findByIdAndDelete(id, (err, doc) => {
+        if (!err && doc) {
+            console.log("Deleted Successfully");
+            res.send(doc);
+        } else {
+            console.error(err);
+            res.send("Unsuccessful Delete");
+        }
+    });
 };
