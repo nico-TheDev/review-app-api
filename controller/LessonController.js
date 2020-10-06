15b2ac2935a7b3d41c36cc1f20daec8215328965
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-06 09:22:09 +0800

    create Lesson Controller

---
+++ controller/LessonController.js
@@ -0,0 +1,65 @@
+const Lesson = require("../models/Lesson");
+
+module.exports.lessons_all_get = async (req, res) => {
+    const { subjectID } = req.params;
+
+    try {
+        const lessons = await Lesson.find({ subjectID });
+        res.json(lessons);
+    }catch(err){
+        console.log(err);
+        res.status(400).send("Error in the server");
+    }
+};
+module.exports.lesson_new_post = async (req, res) => {
+    const { subjectID, title, count, desc } = req.body.data;
+    try {
+        const lesson = await Lesson.create({
+            subjectID,
+            title,
+            count,
+            desc,
+        });
+
+        console.log(lesson);
+        res.send(`New Lesson Created: ${lesson.title}`);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Error in the server");
+    }
+};
+module.exports.lesson_edit_post = (req, res) => {
+    const { subjectID, title, count, desc } = req.body.data;
+    const {id } = req.params;
+
+    const updatedLesson = {
+        subjectID,
+        title,
+        count,
+        desc
+    }
+
+    Lesson.findByIdAndUpdate(id,updatedLesson,(err,doc) =>{
+        if(!err){
+            console.log('Lesson Updated!')
+            res.send(doc);
+        }else{
+            console.log(err);
+            res.status(404).send(err);
+        }
+    })
+
+};
+module.exports.lessons_delete = (req, res) => {
+    const { id } = req.params;
+
+    Lesson.findByIdAndDelete(id,(err,doc) => {
+        if(!err){
+            console.log('Lesson Deleted!')
+            res.send(doc)
+        }else{
+            console.log(err);
+            res.status(404).send(err);
+        }
+    })
+};
