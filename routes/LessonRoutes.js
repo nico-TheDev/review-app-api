32d537bc762a015e415c6e6c0ce665dbf4f20927
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-06 09:22:01 +0800

    create the Lesson Routes

---
+++ routes/LessonRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const lessonController = require("../controller/LessonController");
+
+const router = Router();
+
+const url = (link) => `/subject/:subjectID/lesson${link}`;
+// get lessons
+
+router.get(url("/all"), lessonController.lessons_all_get);
+
+// create new lesson
+router.post(url("/new"), lessonController.lesson_new_post);
+
+//update lesson
+router.post(url("/:id"), lessonController.lesson_edit_post);
+
+//delete lesson
+router.delete(url("/:id"), lessonController.lessons_delete);
+
+module.exports = router;
