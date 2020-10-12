05a29679f8d59c4a998e690f544c6319a1de8ac8
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-12 15:13:22 +0800

    add specific lesson route

--- routes/LessonRoutes.js
+++ routes/LessonRoutes.js
@@ -5,8 +5,11 @@ const router = Router();
 
 const url = (link) => `/subject/:subjectID/lesson${link}`;
 // get lessons
-
 router.get(url("/all"), lessonController.lessons_all_get);
+
+// get specific lesson
+
+router.get(url('/:id'),lessonController.lesson_get)
 
 // create new lesson
 router.post(url("/new"), lessonController.lesson_new_post);
