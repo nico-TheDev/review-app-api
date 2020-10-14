3131bae162773b98e98cde17da501ebf3c523394
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-14 08:49:47 +0800

    add verification middleware

---
+++ middleware/AuthMiddleware.js
@@ -0,0 +1,40 @@
+const User = require("../models/User");
+
+module.exports.validateUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "reviewapp", async (err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+};
+
+module.exports.requireAuth = (req,res,next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, "reviewapp", async (err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login')
+    }
+}
