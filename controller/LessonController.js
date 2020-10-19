48a792ed623bd72fc5034376be7e52962f8af9f6
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-10-19 19:32:01 +0800

    format code

--- controller/LessonController.js
+++ controller/LessonController.js
@@ -6,34 +6,32 @@ module.exports.lessons_all_get = async (req, res) => {
     try {
         const lessons = await Lesson.find({ subjectID });
         res.json(lessons);
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.status(400).send("Error in the server");
     }
 };
 
-module.exports.lesson_get = async  (req,res) => {
-    const {id} = req.params
+module.exports.lesson_get = async (req, res) => {
+    const { id } = req.params;
 
-    try{
-        const lesson = await Lesson.findById(id)
-        res.json(lesson)
+    try {
+        const lesson = await Lesson.findById(id);
+        res.json(lesson);
+    } catch (err) {
+        console.log(err);
+        res.status(404).send(err);
     }
-    catch(err){
-        console.log(err)
-        res.status(404).send(err)
-    }
-
-}   
+};
 
 module.exports.lesson_new_post = async (req, res) => {
-    const { subjectID, title, count, desc } = req.body.data;
+    const { subjectID, title, count, description } = req.body.data;
     try {
         const lesson = await Lesson.create({
             subjectID,
             title,
             count,
-            desc,
+            description,
         });
 
         console.log(lesson);
@@ -44,37 +42,36 @@ module.exports.lesson_new_post = async (req, res) => {
     }
 };
 module.exports.lesson_edit_post = (req, res) => {
-    const { subjectID, title, count, desc } = req.body.data;
-    const {id } = req.params;
+    const { subjectID, title, count, description } = req.body.data;
+    const { id } = req.params;
 
     const updatedLesson = {
         subjectID,
         title,
         count,
-        desc
-    }
+        description,
+    };
 
-    Lesson.findByIdAndUpdate(id,updatedLesson,(err,doc) =>{
-        if(!err){
-            console.log('Lesson Updated!')
+    Lesson.findByIdAndUpdate(id, updatedLesson, (err, doc) => {
+        if (!err) {
+            console.log("Lesson Updated!");
             res.send(doc);
-        }else{
+        } else {
             console.log(err);
             res.status(404).send(err);
         }
-    })
-
+    });
 };
 module.exports.lessons_delete = (req, res) => {
     const { id } = req.params;
 
-    Lesson.findByIdAndDelete(id,(err,doc) => {
-        if(!err){
-            console.log('Lesson Deleted!')
-            res.send(doc)
-        }else{
+    Lesson.findByIdAndDelete(id, (err, doc) => {
+        if (!err) {
+            console.log("Lesson Deleted!");
+            res.send(doc);
+        } else {
             console.log(err);
             res.status(404).send(err);
         }
-    })
+    });
 };
